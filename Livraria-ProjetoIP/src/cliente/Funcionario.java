9f10cdafa7afca36bfb350e8f141cd31bdc39eda
olgapinheiro <olgapin@gmail.com>
2017-06-15 14:59:54 -0300

    Add files via upload

---
+++ Livraria-ProjetoIP/src/cliente/Funcionario.java
@@ -0,0 +1,62 @@
+package cliente;
+
+public class Funcionario extends Pessoa {
+	double salario;
+	double comissao;
+	int vendasMes;
+	// int funcionarioDestaque;
+
+	// construtor
+	public Funcionario(String nome, String cpf) {
+		super(nome, cpf);
+	}
+
+	// getters & setters
+	protected double getSalario() {
+		return salario;
+	}
+
+	protected void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	protected double getComissao() {
+		return comissao;
+	}
+
+	protected void setComissao(double comissao) {
+		this.comissao = comissao;
+	}
+
+	protected int getVendasMes() {
+		return vendasMes;
+	}
+
+	protected void setVendasMes(int vendasMes) {
+		this.vendasMes = vendasMes;
+	}
+	
+	
+	// metodos especificos
+	
+	public void venderProduto(Produto produto){
+		
+	}
+	
+	public void renderComissao(double valorVenda){
+		
+	}
+	
+	public void receberComissao(){
+		
+	}
+	
+	public void aumentarSalario(){
+		
+	}
+	
+	public void ganharDestaque(){
+		
+	}
+
+}
